d73c0a39656718263098881d4dd3db632f51fac9
80997159 <tolson8997@gmail.com>
2015-02-10 11:07:17 -0800

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -9,7 +9,7 @@ public static void main(String[] args){
 			double randx = monteCarlo.nextRainDrop_x();
 			double randy = monteCarlo.nextRainDrop_y();
 			
-			if(monteCarlo.insideCirle(randx, randy)){
+			if(monteCarlo.insideCircle(randx, randy)){
 				cirCount++;
 				
 			}
